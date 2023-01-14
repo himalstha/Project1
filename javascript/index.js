5539ad148f46a8f25a5ad1f4c22a1660f8f375b6
Himal Shrestha <himalstha52@gmail.com>
2023-01-14 19:49:56 +0545

    updated

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,8 @@
 
+ (function (I, L, T, i, c, k, s) { if (I.iticks) return; I.iticks = { host: c, settings: s, clientId: k, cdn: L, queue: [] }; var h = T.head || T.documentElement; var e = T.createElement(i); var l = I.location; e.async = true; e.src = (L || c) + '/client/inject-v2.min.js'; h.insertBefore(e, h.firstChild); I.iticks.call = function (a, b) { I.iticks.queue.push([a, b]); }; })(window, 'https://cdn-v1.intelliticks.com/prod/common', document, 'script', 'https://app.intelliticks.com', 'XoQXxTKFFo36h2CcA_c', {});
+  
 
+ 
 var options = {
       classname: 'my-class',
       id: 'my-id'
